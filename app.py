1f072bcfecb8c5ef66a14c4b929f7211f28fbb2d
이주영 <a65635466@gmail.com>
2026-08-04 10:19:42 +0900

    Add About page and routing

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from app.routes.home import home_bp
+from app.routes.about import about_bp
 
 # Flask 애플리케이션 생성
 app = Flask(__name__)
 
 # Blueprint 등록
 app.register_blueprint(home_bp)
+app.register_blueprint(about_bp)
 
 # 서버 실행
 if __name__=="__main__":
